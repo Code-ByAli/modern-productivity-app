51267d116f088c8902498de5a901b5e4e59cea0a
Code-ByAli <adambouassal@gmail.com>
2025-08-31 02:52:11 +0100

    feat: initialize backend and frontend projects

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-slate-900">
+      <h1 className="text-5xl font-bold text-cyan-400">Modern To-Do App</h1>
+    </div>
+  );
+}
+
+export default App;
